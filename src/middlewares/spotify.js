fb982874c498382d663abb5b8c3582ba4682ca87
wesleydeverdade <wesleymagalhaes5@hotmail.com>
2020-04-11 08:08:10 -0300

    arrumei aquela merda

--- src/middlewares/spotify.js
+++ src/middlewares/spotify.js
@@ -2,26 +2,21 @@ import axios from "axios";
 import qs from "querystring";
 
 class Spotify {
-  constructor() {
+  
+  async auth(){
+    const clientId =  "5e3a5116a53f4354aa4dd0feda2225dd";
+    const clientSecret = "c7be98e714264cad93287bc43bfe8f9d";      
 
-    this.clientId =  "5e3a51 16a53f4354aa4dd0feda2225dd";
-    this.clientSecret = "c7be98e714264cad93287bc43bfe8f9d";  
-
-    this.key = this.auth();
-    this.album = "OK Computer";
-    this.search(this.album);
-  }
-
-  auth = async () => {
     const endpoint = 'https://accounts.spotify.com/api/token';
+
     const headers = {
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/x-www-form-urlencoded',
       },
       auth: {
-        username: this.clientId,
-        password: this.clientSecret,
+        username: clientId,
+        password: clientSecret,
       },
     };
 
@@ -37,26 +32,49 @@ class Spotify {
       );
       return response.data.access_token;
     } catch (error) {
-      return "";
+      return false;
     }
   };
 
-  search(album) {
-    
-    const data = {
-        q:album,
-        type:"album",
-        market:'us',
-        limit:10,
-        offset:5
+  async search(album) {
+
+    if(!album){
+      return false;
     }
 
-    const endpoint = "https://api.spotify.com/v1/search";
+    const key = await this.auth();
+    
+    const endpoint = "https://api.spotify.com/v1/search?";
 
-    console.log(data)
+    axios.defaults.headers.common['Authorization'] = `Bearer ${key}`;
+    const headers = {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+    };
+
+    const data = {
+      q: album,
+      type: "album",
+      market: 'us',
+      limit: 10,
+      offset: 5
+    }
+
+    try {
+      const response = await axios.get(
+        endpoint+qs.stringify(data),
+        headers
+      );
+      //console.log(response.data);
+      return response.data;      
+    } catch (err) {
+      //console.log(err.response);
+      return false;
+    }
 
   }
 }
-
-export default new Spotify();
  
+export default new Spotify().search("Nevermind");
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import spotify from "./middlewares/spotify";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
   return res.json({ message: "Hello teste" });
 });
+
+routes.post("/spotify", spotify.start );
 
 export default routes;
