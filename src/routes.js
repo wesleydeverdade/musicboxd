8473185590e130ca17e959d4c7cc5ddca48ca271
wesleydeverdade <wesleymagalhaes5@hotmail.com>
2020-04-19 19:27:57 -0300

    resolvendo resultados não relevantes e passando um eslint

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,6 @@ routes.get("/", (req, res) => {
   return res.json({ message: "Hello teste" });
 });
 
-routes.post("/spotify", spotify.search );
+routes.post("/spotify", spotify.search);
 
 export default routes;
