296ded77bdd0dc656d02fa16e2b59eba4b4a91dc
wesleydeverdade <wesleymagalhaes5@hotmail.com>
2020-05-26 23:23:59 -0300

    docker

--- docker.sh
+++ docker.sh
@@ -1,11 +1,12 @@
-docker run --name musicbox -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
-docker run --name musicbox -p 27017:27017 -d -t mongo
-docker run --name mysqlmusicbox -e MYSQL_ROOT_PASSWORD=docker -d mysql
+docker run --name pgmusicbox -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
+docker run --name mysqlmusicbox -e MYSQL_ROOT_PASSWORD=docker -p 3306:3306 -d mysql
+docker run --name mongomusicbox -p 27017:27017 -d -t mongo
 docker run --name redismusicbox -p 6379:6379 -d -t redis:alpine
 
 docker start pgmusicbox
-docker start mongomusicbox
 docker start mysqlmusicbox
+docker start mongomusicbox
+docker start redismusicbox
 
 docker container ps -a
 
